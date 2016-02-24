c841987a606d95a566109aa80b653fa87d8069ad
Banjorium <jlafontaine.it@gmail.com>
2016-02-24 08:46:14 -0500

    Created process, changed sitename to use read-host

---
+++ QueryWebsiteInfo.ps1
@@ -0,0 +1,20 @@
+﻿$sitename = Read-Host "Please enter a website in the format: 'http://site.com'"
+
+Begin {
+    $web = New-Object System.Net.WebClient
+    $flag = $false
+    }
+Process {
+    While ($flag -eq $false) {
+        Try {
+            $web.DownloadString($sitename)
+            $flag = $true
+            }
+        Catch
+
+Try {
+    $web.DownloadString($sitename)
+    }
+Catch {
+    Write-Warning "$($Error[0])"
+    }
